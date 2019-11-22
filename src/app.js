e6a9e991556e14b5e37b90569e6c01aa90ab2b62
victor <viheanaco@gmail.com>
2019-11-22 23:05:57 +0100

    send and receive stream from browser to server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +12,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/../views'));
 app.use(express.static(`${path.join(__dirname)}/../public`));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser)
+
 app.get('/', (req, res) => res.render('index'));
+
+app.post('/send_file', upload.single('soundBlob'), (req, res) => {
+  console.log('here', req.body, req.file);
+});
 app.listen(PORT, () => console.log(`serving on port ${PORT}`));
 
 module.exports = app;
